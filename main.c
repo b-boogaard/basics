177d07f304d878cb4714562e6ee2f7d9b729e8f3
Brian van de Boogaard <brian.boogaard@gmail.com>
2019-02-10 15:50:29 -0600

    Create initial project

---
+++ main.c
@@ -0,0 +1,16 @@
+/*!
+ * Main entry point function basic and standard
+ */
+
+#include <stdio.h>
+
+/*!
+ * \brief This is a brief function.
+ * @return 0 on success anything else is a failure.
+ */
+int
+main ()
+{
+  printf ("Hello, World!\n");
+  return 0;
+}
